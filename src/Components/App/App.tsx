6079beac7dd309d88a7b0c10a377ae8424eb04c3
Kirill Kurentsov <immortalsteel@gmail.com>
2023-08-11 12:28:49 +0300

    refactor: Переписал логику карточек на условных рендерах

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,43 +1,137 @@
-import React, { useEffect, useState } from "react";
-import { Cards } from "../Cards/Cards";
+import React from "react";
 import "./App.css";
+import imgFirebase from "../../images/firebase.svg";
+import imgNginx from "../../images/nginx.svg";
+import imgNode from "../../images/node.svg";
+import imgReact from "../../images/react.svg";
+import imgRedux from "../../images/redux.svg";
+import imgTs from "../../images/ts.svg";
+import imgWebpack from "../../images/webpack.svg";
+import imgWs from "../../images/ws.svg";
+import imgBack from "../../images/logo.svg";
+
+export const INITIAL_CARDS: Card[] = [
+  { id: 1, img: imgFirebase, stat: "" },
+  { id: 2, img: imgNginx, stat: "" },
+  { id: 3, img: imgNode, stat: "" },
+  { id: 4, img: imgReact, stat: "" },
+  { id: 5, img: imgRedux, stat: "" },
+  { id: 6, img: imgTs, stat: "" },
+  { id: 7, img: imgWebpack, stat: "" },
+  { id: 8, img: imgWs, stat: "" },
+];
+
+interface Card {
+  id: number;
+  img: string;
+  stat: string;
+}
+
+const deck: Card[] = [...INITIAL_CARDS, ...INITIAL_CARDS];
 
 export function App() {
-  const [steps, setSteps] = useState(0);
-  const avaliableSteps = 4;
-  const [modalVisible, setModalVisible] = useState({
-    status: false,
-    message: "",
-  });
+  const [arrayCards, setArrayCards] = React.useState<Card[]>([]);
+  const [openCards, setOpenCards] = React.useState<number[]>([]);
+  const [matched, setMatched] = React.useState<number[]>([]);
+  const [attempts, setAttempts] = React.useState<number>(40);
+  const [moves, setMoves] = React.useState<number>(0);
 
-  useEffect(() => {
-    if (steps === 0) {
-      setModalVisible({
-        status: true,
-        message: "УВЫ, ВЫ ПРОИГРАЛИ У ВАС КОНЧИЛИСЬ ХОДЫ",
-      });
+  React.useEffect(() => {
+    setArrayCards(deck.sort(() => Math.random() - 0.5));
+  }, []);
+
+  const flipCard = (index: number): void => {
+    if (openCards.length > 1) return;
+    else setOpenCards((openCard) => [...openCard, index]);
+  };
+
+  React.useEffect(() => {
+    if (openCards.length < 2) return;
+
+    const firstMatched = arrayCards[openCards[0]];
+    const secondMatched = arrayCards[openCards[1]];
+    if (secondMatched && firstMatched.id === secondMatched.id) {
+      setMatched([...matched, firstMatched.id]);
     }
-  }, [steps]);
+
+    if (openCards.length === 2) {
+      setMoves(moves + 1);
+      setAttempts(attempts - 1);
+      setTimeout(() => setOpenCards([]), 1500);
+    }
+  }, [openCards]);
+
+  const gameRestart = () => {
+    setOpenCards([]);
+    setMatched([]);
+    setAttempts(40);
+    setMoves(0);
+    setArrayCards(deck.sort(() => Math.random() - 0.5));
+  };
 
   return (
     <div className="App">
       <h1 className="App__title">Memory</h1>
-      <div className="App__board">
+
+      <main className="App__board">
         <div className="App__text">
-          Сделано ходов <span className="App__span">{steps}</span>
+          Сделано ходов <span className="App__span">{moves}</span>
         </div>
-        <Cards
-          steps={steps}
-          setSteps={setSteps}
-          avaliableSteps={avaliableSteps}
-          modalVisible={modalVisible}
-          setModalVisible={setModalVisible}
-        />
+
+        <div className="App__cards">
+          {arrayCards.map((card, index) => {
+            let isFlipped = false;
+            let isVisible = true;
+            if (openCards.includes(index)) isFlipped = true;
+            if (matched.includes(card.id)) {
+              isFlipped = true;
+              isVisible = false;
+            }
+
+            return (
+              <div
+                key={index}
+                onClick={() => flipCard(index)}
+                className={`  ${!isFlipped ? "card" : "flipped card--block"}
+                           ${!isVisible ? "card--hidden" : ""}`}
+              >
+                <div className="card-front ">
+                  <img src={card.img} width="100" alt="card-front" />
+                </div>
+                <div className="card-back ">
+                  <img src={imgBack} width="100" alt="card-back" />
+                </div>
+              </div>
+            );
+          })}
+        </div>
         <div className="App__text">
-          Осталось попыток{" "}
-          <span className="App__span">{avaliableSteps - steps}</span>
+          Осталось попыток <span className="App__span">{attempts}</span>
         </div>
-      </div>
+      </main>
+      {attempts === 0 ? (
+        <div className="modal">
+          <div className="modal-content">
+            <p>Увы, вы проиграли</p>
+            <p>У вас кончились ходы</p>
+          </div>
+          <button onClick={() => gameRestart()} className="modal__button">
+            Сыграть еще
+          </button>
+        </div>
+      ) : null}
+
+      {matched.length === 8 ? (
+        <div className="modal">
+          <div className="modal-content">
+            <p>Ура, вы выйграли</p>
+            <p>{`Это заняло ${moves} ходов`}</p>
+          </div>
+          <button onClick={() => gameRestart()} className="modal__button">
+            Сыграть еще
+          </button>
+        </div>
+      ) : null}
     </div>
   );
 }
---
+++ src/utils.ts
@@ -0,0 +1,27 @@
+import imgFirebase from "./images/firebase.svg";
+import imgNginx from "./images/nginx.svg";
+import imgNode from "./images/node.svg";
+import imgReact from "./images/react.svg";
+import imgRedux from "./images/redux.svg";
+import imgTs from "./images/ts.svg";
+import imgWebpack from "./images/webpack.svg";
+import imgWs from "./images/ws.svg";
+
+export const INITIAL_CARDS = [
+  { id: 1, img: imgFirebase, stat: "" },
+  { id: 1, img: imgFirebase, stat: "" },
+  { id: 2, img: imgNginx, stat: "" },
+  { id: 2, img: imgNginx, stat: "" },
+  { id: 3, img: imgNode, stat: "" },
+  { id: 3, img: imgNode, stat: "" },
+  { id: 4, img: imgReact, stat: "" },
+  { id: 4, img: imgReact, stat: "" },
+  { id: 5, img: imgRedux, stat: "" },
+  { id: 5, img: imgRedux, stat: "" },
+  { id: 6, img: imgTs, stat: "" },
+  { id: 6, img: imgTs, stat: "" },
+  { id: 7, img: imgWebpack, stat: "" },
+  { id: 7, img: imgWebpack, stat: "" },
+  { id: 8, img: imgWs, stat: "" },
+  { id: 8, img: imgWs, stat: "" },
+];
